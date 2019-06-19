468a1656208b86f81bb167e826c2056c4a61e8f2
Daniel <danidiaza@gmail.com>
2019-06-19 11:17:19 -0600

    Rename del Main

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
 import threading
 import logging
 import time
+from riscv import core
 
 
-def run_cpu():
+def run_cpu(cpu):
     logging.info('Starting thread %s', threading.current_thread().getName())
+    logging.info(str(cpu))
     time.sleep(5)
     logging.info('Thread ending %s', threading.current_thread().getName())
 
@@ -15,10 +17,12 @@ def main():
     logging.basicConfig(format=format, level=logging.INFO, datefmt="%H:%M:%S")
 
     # TODO: create data structures
+    core0 = core.Core('CPU0', None)
+    core1 = core.Core('CPU1', None)
 
     # Spawn child Threads
-    t_cpu0 = threading.Thread(target=run_cpu, name='CPU0')
-    t_cpu1 = threading.Thread(target=run_cpu, name='CPU1')
+    t_cpu0 = threading.Thread(target=run_cpu, name='CPU0', args=(core0, ))
+    t_cpu1 = threading.Thread(target=run_cpu, name='CPU1', args=(core1, ))
 
     t_cpu0.start()
     t_cpu1.start()
